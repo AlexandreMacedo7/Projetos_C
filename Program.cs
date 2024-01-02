4734dc31de852f214fb70c30592bc5f8704fd74a
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-01-01 22:28:33 -0400

    Cria classe Pessoa e instancia

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Projetos_C.Models;
+
+Pessoa p = new Pessoa();
+
+p.Nome = "Alexandre";
+p.Idade = 27;
+
+p.Apresentar();
